ef5b3a33115bff2d59f55608af5d135c995fbb13
snegha112-hash <snegha112@gmail.com>
2026-06-26 17:04:43 +0530

    add python

--- Learning_python/Second.py
+++ Learning_python/Second.py
@@ -4,5 +4,45 @@ Age = 22
 City = "Mumbai"
 
 print(Name)
-print(age)
-print()
+print(Age)
+print(City)
+
+#DATA TYPE
+A = 10
+print(type(A))
+b = 10.5
+print(type(b))
+c = "Hello, World!"
+print(type(c))
+d = True
+print(type(d))
+
+#OPERATORS
+print(20 + 10)
+print(20 - 10)
+print(20 * 10)
+print(20 / 10)
+
+#IF...ELSE
+a = 20
+if a > 10:
+    print("positive")
+else:
+    print("not positive")
+
+num = 20
+if num == 20:
+        print("zero")
+else:
+        print("not zero")  
+
+ #LOOPS
+ #FOR LOOP
+for i in range(1, 10):
+        print(i)
+
+for ch in "snegha":
+        print(ch)
+
+for i in [1, 2, 3, 4, 5]:
+        print(i)
